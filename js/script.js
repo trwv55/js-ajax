6e38f861cfba28f6133dda3b7164ff8518ddda3e
Виталий Травин <vtravin@nologostudio.ru>
2023-06-16 11:27:25 +0600

    work with fetch js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
         // });
 
         getResource('http://localhost:3000/people')
-            .then((data) => createCards(data))
+            .then((data) => console.log(data))
             .catch((err) => console.error(err));
 
         this.remove();
@@ -23,14 +23,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('button').addEventListener('click', req, { once: true });
 
-    async function getResource(url) {
-        const res = await fetch(`${url}`);
+    // async function getResource(url) {
+    //     const res = await fetch(`${url}`);
 
-        if (!res.ok) {
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    //     }
+
+    //     return await res.json();
+    // }
+
+    async function getResource(url) {
+        const res = await axios(`${url}`);
+
+        if (res.status !== 200) {
             throw new Error(`Could not fetch ${url}, status ${res.status}`);
         }
 
-        return await res.json();
+        return await res;
     }
 
     function createCards(response) {
